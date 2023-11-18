abd0772b07b6a112bb6bec443ccde181319318a1
Philippa Wisniewski <pecfwisniewski@gmail.com>
2023-11-18 16:36:23 +0000

    Adding active tab to change the colour of the tabs

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,17 @@
+'use client';
 import Image from 'next/image';
 import styles from '../styles/components/header.module.scss';
 import hackerNewsLogo from '../images/hacker-news-logo.svg';
+import { usePathname } from 'next/navigation';
 
 const TabLink: React.FC<{ href: string; label: string }> = ({
   href,
   label
 }) => {
+ const activeTab = usePathname() === href ? `${styles.active_tab}` : '';
+
   return (
-    <a href={href} className={styles.tab_link}>
+    <a href={href} className={activeTab}>
       {label}
     </a>
   );
