41c681e1694179d0ab463d4179bc954a4cdadc49
Philippa Wisniewski <pecfwisniewski@gmail.com>
2023-11-19 22:53:32 +0000

    adding dark-toggle svg and light-toggle svg, adding some changes to the styling, adding a theme provider for the dark theme toggle, changing layout page and adding  the changes to app page so the theme provider works

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,18 @@
-'use client';
-import Image from 'next/image';
-import styles from '../styles/components/header.module.scss';
-import hackerNewsLogo from '../images/hacker-news-logo.svg';
-import { usePathname } from 'next/navigation';
+"use client";
+import { useContext } from "react";
+import Image from "next/image";
+import styles from "../styles/components/header.module.scss";
+import hackerNewsLogo from "../images/hacker-news-logo.svg";
+import { usePathname } from "next/navigation";
+import darkToggle from "../images/dark-toggle.svg";
+import lightToggle from "../images/light-toggle.svg";
+import { ThemeContext } from "../themeProvider";
 
 const TabLink: React.FC<{ href: string; label: string }> = ({
   href,
-  label
+  label,
 }) => {
- const activeTab = usePathname() === href ? `${styles.active_tab}` : '';
+  const activeTab = usePathname() === href ? `${styles.active_tab}` : "";
 
   return (
     <a href={href} className={activeTab}>
@@ -17,7 +21,25 @@ const TabLink: React.FC<{ href: string; label: string }> = ({
   );
 };
 
+const ToggleImage: React.FC<{
+  isDarkmode: boolean;
+  setIsDarkmode: () => void;
+}> = ({ isDarkmode, setIsDarkmode }) => {
+  return (
+    <Image
+      onClick={setIsDarkmode}
+      className={isDarkmode ? styles.light_toggle : styles.dark_toggle}
+      src={isDarkmode ? lightToggle : darkToggle}
+      alt="dark mode toggle"
+      priority
+    />
+  );
+};
+
+
+
 export default function Header() {
+  const { isDarkmode,  toggleTheme} = useContext(ThemeContext);
   return (
     <div className={styles.header}>
       <Image
@@ -27,9 +49,10 @@ export default function Header() {
         priority
       />
       <div className={styles.tabs}>
-        <TabLink href="/" label="latest" /> |{' '}
+        <TabLink href="/" label="latest" /> |{" "}
         <TabLink href="/starred" label="starred" />
       </div>
+      <ToggleImage isDarkmode={isDarkmode} setIsDarkmode={toggleTheme} />
     </div>
   );
 }
